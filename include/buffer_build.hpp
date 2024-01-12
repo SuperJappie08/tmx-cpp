a65db47f8204c0269ea1eb5be1f5847f2dccc864
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-01-12 12:32:35 +0000

    cpplint fixes

--- include/buffer_build.hpp
+++ include/buffer_build.hpp
@@ -1,13 +1,14 @@
-#include <algorithm>
-#include <iostream>
 #include <stdint.h>
 #include <stdio.h>
+#include <algorithm>
+#include <iostream>
 #include <vector>
 
 typedef std::vector<uint8_t> byte_buffer;
 
-template <std::size_t N>
-void append_fixed_width(byte_buffer &buf, uintmax_t val) {
+template<std::size_t N>
+void append_fixed_width(byte_buffer & buf, uintmax_t val)
+{
   int shift = ((N - 1) * 8);
   while (shift >= 0) {
     uintmax_t mask = (0xff << shift);
@@ -16,26 +17,30 @@ void append_fixed_width(byte_buffer &buf, uintmax_t val) {
   }
 }
 
-template <typename IntType> void append_bytes(byte_buffer &buf, IntType val) {
+template<typename IntType> void append_bytes(byte_buffer & buf, IntType val)
+{
   append_fixed_width<sizeof(IntType)>(buf, uintmax_t(val));
 }
 
-template <typename T> void append_buffer(byte_buffer &buf, T t) {
+template<typename T> void append_buffer(byte_buffer & buf, T t)
+{
   append_bytes(buf, t);
 }
 
-template <typename T, typename... Args>
-void append_buffer(byte_buffer &buf, T t,
-                   Args... args) // recursive variadic function
+template<typename T, typename ... Args>
+void append_buffer(
+  byte_buffer & buf, T t,
+  Args... args)                  // recursive variadic function
 {
   append_bytes(buf, t);
 
-  append_buffer(buf, args...);
+  append_buffer(buf, args ...);
 }
-template <typename... Args> byte_buffer append_buffer(Args... args) {
+template<typename ... Args> byte_buffer append_buffer(Args... args)
+{
   byte_buffer buf;
   // append_fixed_width(buf, t);
 
-  append_buffer(buf, args...);
+  append_buffer(buf, args ...);
   return buf;
 }
