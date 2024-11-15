8c7c7043af5e1c36aeb4964f65580178e85fd8e5
SuperJappie08 <36795178+SuperJappie08@users.noreply.github.com>
2024-11-15 16:12:53 +0100

    Scoping of Message type enums

--- include/tmx_cpp/tmx.hpp
+++ include/tmx_cpp/tmx.hpp
@@ -14,6 +14,7 @@
 
 #include "async_serial/AsyncSerial.h"
 
+#include "tmx_cpp/message_types.hpp"
 #include "tmx_cpp/modules.hpp"
 #include "tmx_cpp/sensors.hpp"
 #include "tmx_cpp/types.hpp"
@@ -27,10 +28,6 @@ using callback_func_pin16 = std::function<void(uint8_t, uint16_t)>;
 using callback_func_pin_int = std::function<void(uint8_t, int8_t)>;
 
 class TMX {
-public:
-  enum MESSAGE_IN_TYPE : uint8_t;
-  enum MESSAGE_TYPE : uint8_t;
-
 public:
   std::vector<uint8_t> buffer;
   // normal callbacks
@@ -72,66 +69,6 @@ public:
 public:
   TMX(std::function<void()> stop_func, std::string port = "/dev/ttyACM0");
   ~TMX();
-  enum MESSAGE_TYPE : uint8_t {
-    SERIAL_LOOP_BACK = 0,
-    SET_PIN_MODE = 1,
-    DIGITAL_WRITE = 2,
-    PWM_WRITE = 3,
-    MODIFY_REPORTING = 4,
-    FIRMWARE_VERSION = 5,
-    GET_PICO_UNIQUE_ID = 6,
-    SERVO_ATTACH = 7,
-    SERVO_WRITE = 8,
-    SERVO_DETACH = 9,
-    I2C_BEGIN = 10,
-    I2C_READ = 11,
-    I2C_WRITE = 12,
-    SONAR_NEW = 13,
-    DHT_NEW = 14,
-    STOP_ALL_REPORTS = 15,
-    ENABLE_ALL_REPORTS = 16,
-    RESET_DATA = 17,
-    RESET_BOARD = 18,
-    INITIALIZE_NEO_PIXELS = 19,
-    SHOW_NEO_PIXELS = 20,
-    SET_NEO_PIXEL = 21,
-    CLEAR_ALL_NEO_PIXELS = 22,
-    FILL_NEO_PIXELS = 23,
-    SPI_INIT = 24,
-    SPI_WRITE = 25,
-    SPI_READ = 26,
-    SPI_SET_FORMAT = 27,
-    SPI_CS_CONTROL = 28,
-    SET_SCAN_DELAY = 29,
-    ENCODER_NEW = 30,
-    SENSOR_NEW = 31,
-    PING = 32,
-    MODULE_NEW = 33,
-    MODULE_DATA = 34,
-    GET_ID = 35,
-    SET_ID = 36,
-  };
-  enum MESSAGE_IN_TYPE : uint8_t {
-    SERIAL_LOOP_BACK_REPORT = 0,
-    DIGITAL_REPORT = 2,
-    ANALOG_REPORT = 3,
-    FIRMWARE_REPORT = MESSAGE_TYPE::FIRMWARE_VERSION,
-    REPORT_PICO_UNIQUE_ID = 6,
-    SERVO_UNAVAILABLE = 7, // for the future
-    I2C_WRITE_REPORT = 8,
-    I2C_READ_FAILED = 9,
-    I2C_READ_REPORT = 10,
-    SONAR_DISTANCE = 11,
-    DHT_REPORT = 12,
-    SPI_REPORT = 13,
-    ENCODER_REPORT = 14,
-    DEBUG_PRINT = 99,
-    SENSOR_REPORT = 20,
-    PONG_REPORT = 32,
-    MODULE_REPORT = 34,
-    GET_ID_REPORT = MESSAGE_TYPE::GET_ID,
-    SET_ID_REPORT = MESSAGE_TYPE::SET_ID,
-  };
   enum PIN_MODES : uint8_t {
     DIGITAL_INPUT = 0,
     DIGITAL_OUTPUT = 1,
@@ -143,7 +80,7 @@ public:
   void callback(const char *data, size_t len);
   void sendPing(uint8_t num = 0);
   void sendMessage(const std::vector<uint8_t> &message);
-  void sendMessage(TMX::MESSAGE_TYPE type, const std::vector<uint8_t> &message);
+  void sendMessage(MESSAGE_TYPE type, const std::vector<uint8_t> &message);
   // Normal functions for use by the user:
   void setPinMode(uint8_t pin, TMX::PIN_MODES mode, bool reporting = true,
                   uint16_t analog_differential = 0);
